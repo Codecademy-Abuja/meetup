c82630ee83aa0329ee7fc06457e423030f64a270
Ayodeji Ogundare <hellodejisol@gmail.com>
2021-05-02 04:38:30 +0100

    Add 'basic-calculator.py'.

---
+++ 01-05-21/basic-calculator.py
@@ -0,0 +1,15 @@
+#This is a basic Python calculator
+#From Codecademy Abuja Meetup
+
+#get input from user and convert to int
+number1 = input("Enter first number: \n")
+number1 = int(number1)
+
+number2 = input("Second number: \n")
+number2 = int(number2)
+
+
+result = number1 + number2
+
+#print result
+print(f"Your result is: {result}")
